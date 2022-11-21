8a1203dcac72499f23aea357d3153450ba03bd18
Sarthak <saryatnam22@gmail.com>
2022-11-21 13:55:59 +0530

    updated transfar modal

--- components/modal/TransferModal.js
+++ components/modal/TransferModal.js
@@ -10,7 +10,7 @@ const TransferModal = ({sanityTokens, thirdWebTokens, walletAddress}) => {
     const [action, setAction] = useState('send')
     const [selectedToken, setSelectedToken] = useState(sanityTokens[0])
 
-    // console.log(sanityTokens,"sanity tokens from transfer modal")
+    console.log(sanityTokens,"sanity tokens from transfer modal")
     
     const selectedStyle = {
         color: '#3773f5'
